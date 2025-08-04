235bdcf434ba0d1ccefaf89793a45b0389844dbd
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-04 18:16:35 -0300

    feat(ui): añade botón de cierre de sesión al Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,16 @@ const Header = () => {
       <div>
         <nav>
           <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
             <Link to="/dashboard">Dashboard</Link>
           </li>
           <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
             <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <button>Cerrar Sesión</button>
           </li>
         </nav>
       </div>
