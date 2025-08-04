374423d457e4e0577bc15b227fc738fce71d9665
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-04 15:55:45 -0300

    feat: Añadir componente RouterApp

---
+++ src/router/RouterApp.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "../views/Home"
+import { Login } from "../views/Login"
+import { Register } from "../views/Register"
+import { Dashboard } from "../views/Dashboard"
+import { NotFound } from "../views/NotFound"
+
+const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/login" element={Login} />
+        <Route path="/register" element={Register} />
+        <Route path="/dashboard" element={Dashboard} />
+        <Route path="*" element={NotFound} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export { RouterApp }
