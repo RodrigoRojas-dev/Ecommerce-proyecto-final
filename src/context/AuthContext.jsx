74d2dfdd4372da4e42f35e30d2f708c364bed1f5
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-08 17:50:31 -0300

    feat: Implementar la funcionalidad de inicio de sesión

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,8 +2,33 @@ import { createContext, useContext, useState } from "react"
 
 const AuthContext = createContext()
 
-const AuthProvider = () => {
+const AuthProvider = (props) => {
   const [user, setUser] = useState(null)
 
+  const loginUser = async (username, password) => {
+    const response = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, password })
 
+
+    })
+
+    if (response.ok) {
+      const token = await response.json()
+      setUser(true)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
 }
+
+const useAuth = () => useContext(AuthContext)
+
+export { AuthProvider, useAuth }
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,26 +1,34 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 const Login = () => {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
+  const { loginUser } = useAuth()
 
   console.log(username, password);
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    loginUser(username, password)
+    setUsername("")
+    setPassword("")
+  }
 
   return (
     <>
       <section>
         <h1>Acceder</h1>
         <h2>Inicia Sesión para continuar.</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Nombre de Usuario</label>
-            <input type="text" onChange={(e) => { setUsername(e.target.value) }} />
+            <input type="text" onChange={(e) => { setUsername(e.target.value) }} value={username} />
           </div>
           <div>
             <label>Contraseña</label>
-            <input type="password" onChange={(e) => { setPassword(e.target.value) }} />
+            <input type="password" onChange={(e) => { setPassword(e.target.value) }} value={username} />
           </div>
           <div>
             <button>Acceder</button>
