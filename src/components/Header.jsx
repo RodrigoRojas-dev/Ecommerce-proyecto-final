b8f6cb8824c83bb1f96065577d92b71fa62fb693
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-08 18:00:58 -0300

    feat: Actualizar el nav del Header según el estado de autenticación

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 const Header = () => {
+  const { user } = useAuth()
+
   return (
     <header>
       <div>
         <Link to="/">Home</Link>
       </div>
       <div>
-        <nav>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <button>Cerrar Sesión</button>
-          </li>
-        </nav>
-      </div>
-    </header>
+        {
+          user &&
+          <nav>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <button>Cerrar Sesión</button>
+            </li>
+          </nav>
+        }
+        {
+          !user &&
+          <nav>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </nav>
+        }
+    </div>
+    </header >
   )
 }
 
