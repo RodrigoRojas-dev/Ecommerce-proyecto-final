04b13d0322616955def84ad0904c083c547d0ff3
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-09 20:28:56 -0300

    feat: Crear contenido estático para el componente PopUp

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -2,4 +2,36 @@
 
 const PopUp = () => {
 
+  return (
+    <section>
+      <div>
+        <h2>Crear Nuevo Producto</h2>
+        <div>
+          <img src="" alt="" />
+          <form>
+            <div>
+              <label>Nombre del Producto</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Precio</label>
+              <input type="number" />
+            </div>
+            <div>
+              <label>Descripción</label>
+              <textarea></textarea>
+            </div>
+            <div>
+              <label>Categoria</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Imagen</label>
+              <input type="url" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }
