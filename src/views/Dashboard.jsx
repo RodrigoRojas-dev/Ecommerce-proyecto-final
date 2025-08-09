1ba603888a963cf2e9c8b7c74eabefc5a4bc35dc
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-09 18:50:59 -0300

    feat: Reemplazar contenido temporal del Dashboard por estático (parcial)

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,6 +1,32 @@
 const Dashboard = () => {
   return (
-    <h1>Hola desde el Dashboard</h1>
+    <>
+      <section>
+        <form>
+          <div>
+            <label>Nombre del Producto</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Precio</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>Descripción</label>
+            <textarea></textarea>
+          </div>
+          <div>
+            <label>Categoria</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>URL de la Imagen</label>
+            <input type="url" />
+          </div>
+          <button>Añadir Producto</button>
+        </form>
+      </section>
+    </>
   )
 }
 
