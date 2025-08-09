fa2c630880fd3c763147f3e67400a548071b200d
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-09 19:14:57 -0300

    feat: Crear función para obtener productos del API en ProductContext

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -3,6 +3,27 @@ import { createContext, useContext, useState, useEffect } from "react"
 
 const ProductContext = createContext()
 
-const ProductProvider = () => {
+const ProductProvider = (props) => {
+  const [products, setProducts] = useState([])
 
+  const getProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products", { method: "GET" })
+    const data = await response.json()
+
+    setProducts(data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  return (
+    <ProductContext.Provider value={{ products, getProducts }}>
+      {props.children}
+    </ProductContext.Provider>
+  )
 }
+
+const useProducts = () => useContext(ProductContext)
+
+export { ProductProvider, useProducts }
