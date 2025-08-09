50b29e58159382ee588e8b48e66b4f899cf13b4b
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-09 19:55:24 -0300

    feat: Eliminar formulario de nuevo producto del Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -5,31 +5,6 @@ const Dashboard = () => {
   return (
     <>
       <section>
-        <form>
-          <div>
-            <label>Nombre del Producto</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>Precio</label>
-            <input type="number" />
-          </div>
-          <div>
-            <label>Descripción</label>
-            <textarea></textarea>
-          </div>
-          <div>
-            <label>Categoria</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label>URL de la Imagen</label>
-            <input type="url" />
-          </div>
-          <button>Añadir Producto</button>
-        </form>
-      </section>
-      <section>
         {
           products.map((product) => <div key={product.id}>
             <img src={product.image} alt={`Imagen de ${product.title}`} />
