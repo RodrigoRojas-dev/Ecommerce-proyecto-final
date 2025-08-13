5bbe01207ae0438cc1652af94b100bbee9e66edc
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-13 19:11:14 -0300

    feat: actualiza el footer con información de la tienda, enlaces y contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
     <footer>
-      <p>&#xA9; 2025 <a href="https://github.com/RodrigoRojas-dev/">Rodrigo Rojas</a>. Todos los derechos reservados.</p>
+      <div>
+        <h3>Atenas Collection</h3>
+        <p>Tu destino de moda en línea, donde la elegancia se encuentra con la atemporalidad.</p>
+      </div>
+      <div>
+        <h3>Enlaces de Interés</h3>
+        <ul>
+          <li><Link to={"/"}>Inicio</Link></li>
+          <li><Link to={"/sobre-nosotros"}>Sobre Nosotros</Link></li>
+          <li><Link to={"/login"}>Iniciar Sesión</Link></li>
+          <li><Link to={"/register"}>Registrarse</Link></li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contacto</h3>
+        <p>Desarrollado por <a href="https://github.com/RodrigoRojas-dev">Gabriel Alberini</a></p>
+      </div>
+      <p>&#xA9; 2025 Atenas Collection. Todos los derechos reservados.</p>
     </footer>
   )
 }
