a1c1fa9e5a31c16dea12b09e15bf2ddeee957dd0
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-13 15:42:40 -0300

    feat: implementa el filtro de búsqueda para los productos del Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,9 +1,16 @@
 import { PopUp } from "../components/PopUp"
 import { useProducts } from "../context/ProductContext"
+import { useSearch } from "../context/SearchContext"
 
 const Dashboard = () => {
   const { products, delProducts, openPopUp, openToEdit } = useProducts()
+  const { search } = useSearch()
 
+  const filteredProducts = products.filter(product => {
+    const term = search.toLowerCase();
+    const results = product.title.toLowerCase().includes(term)
+    return results
+  });
 
   return (
     <>
@@ -12,8 +19,8 @@ const Dashboard = () => {
         <PopUp />
       </section>
       <section>
-        {
-          products.map((product) => <div key={product.id}>
+        {filteredProducts.length > 0 ?
+          (filteredProducts.map((product) => <div key={product.id}>
             <img src={product.image} alt={`Imagen de ${product.title}`} />
             <h2>{product.title}</h2>
             <p>${product.price}</p>
@@ -21,7 +28,17 @@ const Dashboard = () => {
               <button onClick={() => openToEdit(product)}>Actualizar</button>
               <button onClick={() => delProducts(product.id)}>Borrar</button>
             </div>
-          </div>)
+          </div>))
+          :
+          (products.map((product) => <div key={product.id}>
+            <img src={product.image} alt={`Imagen de ${product.title}`} />
+            <h2>{product.title}</h2>
+            <p>${product.price}</p>
+            <div>
+              <button onClick={() => openToEdit(product)}>Actualizar</button>
+              <button onClick={() => delProducts(product.id)}>Borrar</button>
+            </div>
+          </div>))
         }
       </section>
     </>
