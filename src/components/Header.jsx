d5cd09e57b658a039c37d2f810aee7b5bc274b7c
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-14 09:07:54 -0300

    feat: Implementar menú de navegación responsive con estilos iniciales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 import { SearchBar } from "./SearchBar"
 import AtenasCollection from "../assets/Atenas-Collection.png"
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { user, logoutUser } = useAuth()
 
   const handleLogout = () => {
@@ -13,32 +16,82 @@ const Header = () => {
   return (
     <header className="main-header">
       <div>
-        <Link to="/" className="logo-conteiner"><img src={AtenasCollection} alt="Logo de Atenas Collection" className="logo"/></Link>
+        <Link to="/" className="logo-conteiner"><img src={AtenasCollection} alt="Logo de Atenas Collection" className="logo" /></Link>
       </div>
-      <SearchBar />
-      <div>
-        {
-          user &&
-          <nav>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>Cerrar Sesión</button>
-            </li>
-          </nav>
-        }
-        {
-          !user &&
-          <nav>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </nav>
-        }
+
+      <div className="mobile-menu-btn z-50">
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          {
+            isMenuOpen
+              ?
+              <HiX className="text-4xl" />
+              :
+              <HiMenu className="text-4xl" />
+          }
+        </button>
+      </div>
+
+      {/* Desktop Menu */}
+      <div className="desktop-nav">
+        <SearchBar />
+        <div>
+          {
+            user &&
+            <nav>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Cerrar Sesión</button>
+              </li>
+            </nav>
+          }
+          {
+            !user &&
+            <nav>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </nav>
+          }
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`off-canvas-menu
+          ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        <SearchBar />
+        <div className="desktop:hidden">
+          {
+            user &&
+            <nav>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Cerrar Sesión</button>
+              </li>
+            </nav>
+          }
+          {
+            !user &&
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </ul>
+            </nav>
+          }
+        </div>
       </div>
     </header >
   )
