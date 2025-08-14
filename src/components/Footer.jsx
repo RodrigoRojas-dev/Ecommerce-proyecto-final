cb241f6dc44e7a73965a77e38122785d747714e3
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-14 15:50:27 -0300

    style: Añadir detalles mínimos de estilización al Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,15 @@ const Footer = () => {
       <div>
         <h3 className="h3-footer">Enlaces de Interés</h3>
         <ul>
-          <li><Link to={"/"}>Inicio</Link></li>
-          <li><Link to={"/sobre-nosotros"}>Sobre Nosotros</Link></li>
-          <li><Link to={"/login"}>Iniciar Sesión</Link></li>
-          <li><Link to={"/register"}>Registrarse</Link></li>
+          <li><Link to={"/"} className="link-footer">Inicio</Link></li>
+          <li><Link to={"/sobre-nosotros"} className="link-footer">Sobre Nosotros</Link></li>
+          <li><Link to={"/login"} className="link-footer">Iniciar Sesión</Link></li>
+          <li><Link to={"/register"} className="link-footer">Registrarse</Link></li>
         </ul>
       </div>
       <div>
         <h3 className="h3-footer">Contacto</h3>
-        <p>Desarrollado por <a href="https://github.com/RodrigoRojas-dev">Rodrigo Rojas</a></p>
+        <p>Desarrollado por <a href="https://github.com/RodrigoRojas-dev" className="link-footer">Rodrigo Rojas</a></p>
         <p>&#xA9; 2025 Atenas Collection. Todos los derechos reservados.</p>
       </div>
     </footer>
