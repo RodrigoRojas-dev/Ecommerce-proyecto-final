2c8fd37a0cf155ba6b8bd8b92152883ba8ec67e0
RodrigoRojas-dev <rojas.rodrigo.dev@gmail.com>
2025-08-14 16:03:48 -0300

    style: Añadir estilos iniciales al grid de productos en el Dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -19,27 +19,45 @@ const Dashboard = () => {
         <PopUp />
       </section>
       <section>
-        {filteredProducts.length > 0 ?
-          (filteredProducts.map((product) => <div key={product.id}>
-            <img src={product.image} alt={`Imagen de ${product.title}`} />
-            <h2>{product.title}</h2>
-            <p>${product.price}</p>
-            <div>
-              <button onClick={() => openToEdit(product)}>Actualizar</button>
-              <button onClick={() => delProducts(product.id)}>Borrar</button>
-            </div>
-          </div>))
-          :
-          (products.map((product) => <div key={product.id}>
-            <img src={product.image} alt={`Imagen de ${product.title}`} />
-            <h2>{product.title}</h2>
-            <p>${product.price}</p>
-            <div>
-              <button onClick={() => openToEdit(product)}>Actualizar</button>
-              <button onClick={() => delProducts(product.id)}>Borrar</button>
-            </div>
-          </div>))
+        {
+          filteredProducts.length === 0 && <p className="text-xl text-red-500">No se encontraron resultados</p>
         }
+        <div className="grid-products">
+          {
+            filteredProducts.length > 0 ?
+              (filteredProducts.map((product) => <div key={product.id} className="grid-cards">
+                <div className="bg-img">
+                  <img src={product.image} alt={`Imagen de ${product.title}`} className="img-cards" />
+                </div>
+                <div className="info-card">
+                  <div>
+                    <h3 className="name">{product.title}</h3>
+                    <p className="precio">${product.price}</p>
+                  </div>
+                  <div>
+                    <button onClick={() => openToEdit(product)}>Actualizar</button>
+                    <button onClick={() => delProducts(product.id)}>Borrar</button>
+                  </div>
+                </div>
+              </div>))
+              :
+              (products.map((product) => <div key={product.id} className="grid-cards">
+                <div className="bg-img">
+                  <img src={product.image} alt={`Imagen de ${product.title}`} className="img-cards" />
+                </div>
+                <div className="info-card">
+                  <div>
+                    <h3 className="name">as{product.title}</h3>
+                    <p className="precio">${product.price}</p>
+                  </div>
+                  <div>
+                    <button onClick={() => openToEdit(product)}>Actualizar</button>
+                    <button onClick={() => delProducts(product.id)}>Borrar</button>
+                  </div>
+                </div>
+              </div>))
+          }
+        </div>
       </section>
     </>
   )
